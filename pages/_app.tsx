b46574ccfa68790d986a46fa062609a58b894607
rinku.chaudhary <rinku.chaudhary@amniltech.com>
2022-10-27 18:58:14 +0545

    page title update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,20 @@
-import '../styles/globals.css'
-import type {AppProps} from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 import Head from "next/head";
 
-function MyApp({Component, pageProps}: AppProps) {
-    return <>
-        <Head>
-            <title>Ar3share</title>
-            <meta title='description' content='share files instantly with Ar3share'/>
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-            <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"/>
-        </Head>
-        <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Arcshare</title>
+        <meta title="description" content="share files instantly with Ar3share" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
     </>
+  );
 }
 
-export default MyApp
+export default MyApp;
