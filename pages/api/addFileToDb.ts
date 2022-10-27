75c2b75742e32d77cf81c9c245b1427f321784ef
rinku.chaudhary <rinku.chaudhary@amniltech.com>
2022-10-27 15:48:29 +0545

    ts error fix

--- pages/api/addFileToDb.ts
+++ pages/api/addFileToDb.ts
@@ -1,27 +1,28 @@
-import type, {NextApiRequest, NextApiResponse} from 'next'
+import type, { NextApiRequest, NextApiResponse } from "next";
 import uniqid from "uniqid";
-import db from "../../db"
+import db from "../../db";
 
 type Data = {
-    success: boolean;
-    fileName? : string;
-    fileLocation? : string
-    message? : string
-}
+  success: boolean;
+  fileName?: string;
+  fileLocation?: string;
+  message?: string;
+  fileId?: string;
+};
 
 export default function handler(request: NextApiRequest, response: NextApiResponse<Data>) {
-    const {downloadUrl, fileName} = request.body;
+  const { downloadUrl, fileName } = request.body;
 
-    try {
-        const query = `INSERT INTO files(file_name, file_id, file_location) VALUES ('${fileName}','${uniqid()}','${downloadUrl}') returning file_id`;
-        db.query(query).then((res) => response.status(200).json({success: true, fileId: res.rows[0]?.file_id}));
-    } catch (err: any) {
-        response.status(200).json({success: false, message: err.message});
-    }
+  try {
+    const query = `INSERT INTO files(file_name, file_id, file_location) VALUES ('${fileName}','${uniqid()}','${downloadUrl}') returning file_id`;
+    db.query(query).then((res) => response.status(200).json({ success: true, fileId: res.rows[0]?.file_id }));
+  } catch (err: any) {
+    response.status(200).json({ success: false, message: err.message });
+  }
 }
 
 export const config = {
-    api: {
-        bodyParser: true
-    },
-}
+  api: {
+    bodyParser: true,
+  },
+};
